e7dbc283018936bc90f5789cdaf784f50896020c
Erik <e.fakin3@gmail.com>
2023-12-10 00:20:20 +0100

    day 8 part 2

--- src/main/kotlin/day8/part2/Main.kt
+++ src/main/kotlin/day8/part2/Main.kt
@@ -1,107 +1,130 @@
 package day8.part2
 
-import java.awt.List
 import java.io.File
-import java.util.Arrays
 import kotlin.io.path.Path
-import kotlin.math.*
 
+class Node(val label: String, val leftNodeLabel: String, val rightNodeLabel: String) {
+    override fun toString(): String {
+        return "Node $label - left node : $leftNodeLabel, right node $rightNodeLabel | "
+    }
+}
 
-val cardsScore = "J23456789TQKA"
+fun findLCM(inputArray: IntArray): Long {
+    var lcm: Long = 1
+    var divisor = 2
+    while (true) {
+        var counter = 0
+        var divisible = false
+        for (i in inputArray.indices) {
 
-val handCardsComparator = Comparator<Hand> { hand1, hand2 ->
-    val cards1 = hand1.cards.toCharArray()
-    val cards2 = hand2.cards.toCharArray()
-    var index = 0
-    while (index < cards1.size && index < cards2.size) {
-        val str1Char = cardsScore.indexOf(cards1[index])
-        val str2Char = cardsScore.indexOf(cards2[index])
+            // lcm_of_array_elements (n1, n2, ... 0) = 0.
+            // For negative number we convert into
+            // positive and calculate lcm_of_array_elements.
+            if (inputArray[i] == 0) {
+                return 0
+            } else if (inputArray[i] < 0) {
+                inputArray[i] = inputArray[i] * -1
+            }
+            if (inputArray[i] == 1) {
+                counter++
+            }
 
-        if (str1Char < str2Char) {
-            return@Comparator -1
-        } else if (str1Char > str2Char) {
-            return@Comparator 1
+            // Divide element_array by devisor if complete
+            // division i.e. without remainder then replace
+            // number with quotient; used for find next factor
+            if (inputArray[i] % divisor == 0) {
+                divisible = true
+                inputArray[i] = inputArray[i] / divisor
+            }
         }
 
+        // If divisor able to completely divide any number
+        // from array multiply with lcm_of_array_elements
+        // and store into lcm_of_array_elements and continue
+        // to same divisor for next factor finding.
+        // else increment divisor
+        if (divisible) {
+            lcm *= divisor
+        } else {
+            divisor++
+        }
+
+        // Check if all element_array is 1 indicate
+        // we found all factors and terminate while loop.
+        if (counter == inputArray.size) {
+            return lcm
+        }
+    }
+}
+
+
+
+fun readFile(filename: String): Long {
+    var sum = 0
+    val input = File(filename).readLines()
+    val instructions = input[0]
+    val nodesList: MutableMap<String, List<String>> = mutableMapOf()
+
+    println(instructions)
+
+    input.subList(2, input.size).forEach { line ->
+        val results = Regex("(\\w{3})").findAll(line).toList()
+        val nodeLabel = results[0].groupValues.first()
+        val leftNodeLabel = results[1].groupValues.first()
+        val rightNodeLabel = results[2].groupValues.first()
+        nodesList[nodeLabel] = listOf(leftNodeLabel, rightNodeLabel)
+    }
+
+
+
+    var startingNodes = nodesList.filterKeys { key -> key.endsWith('a', true) }
+    println("Starint from $startingNodes")
+    var currentInstructionIndex = 0
+
+    println("All nodes $nodesList")
+
+    /***find the numbers of steps to reach  the end for every path,
+    we know that every path after it reaches the end starts at the begginign ***/
+
+    val stepsPerNode: MutableList<Int> = MutableList(startingNodes.size) { 0 }
+    var index = 0
+    startingNodes.forEach{startingNode ->
+        println(startingNode)
+        currentInstructionIndex = 0
+        var currentNode = startingNode
+        var nextNode = currentNode
+        while (true) {
+            val instruction = instructions[currentInstructionIndex]
+
+            if (instruction == 'L') {
+               nextNode = nodesList.filterKeys { key -> key == currentNode.value[0] }.entries.first()
+            } else {
+                nextNode = nodesList.filterKeys { key -> key == currentNode.value[1] }.entries.first()
+            }
+
+            if(nextNode.key.endsWith('z', true)) {
+                stepsPerNode[index]++
+                break
+            }
+
+
+
+            currentInstructionIndex = (currentInstructionIndex + 1) % instructions.length
+            currentNode = nextNode
+            stepsPerNode[index]++
+
+        }
         index++
     }
-    return@Comparator cards1.size - cards2.size
-}
 
-val cardsComparator = Comparator<Char> { card1, card2 ->
-    val card1Value = cardsScore.indexOf(card1)
-    val card2Value = cardsScore.indexOf(card2)
-    if (card1Value < card2Value) {
-        return@Comparator -1
-    } else if (card1Value > card2Value) {
-        return@Comparator 1
-    }
-    return@Comparator 0
-}
-
-
-class Hand(val cards: String, val bid: Int) {
-    var score: String = "0000"
-
-    init {
-        calculateScore()
-    }
-
-    private fun calculateScore() {
-
-        val sortedCards = cards.toCharArray().sorted()
-        val cardsMap: MutableMap<Char, Int> = mutableMapOf()
-        for (char in sortedCards) {
-            if (cardsMap[char] == 0) continue
-            if (char == 'J') continue
-            cardsMap[char] = sortedCards.count { c: Char -> c == char }
-        }
-        if (cardsMap.isNotEmpty()) {
-            val maxScoreCard =
-                cardsMap.filterValues { it == cardsMap.values.max() }.keys.sortedWith(cardsComparator).reversed()
-                    .first()
-            val jokers = sortedCards.count { c: Char -> c == 'J' }
-
-            cardsMap[maxScoreCard] = cardsMap[maxScoreCard]!! + jokers
-
-        } else cardsMap['J'] = 5
-
-        for (i in 2..5) {
-            score = score.replaceRange(i - 2, i - 1, cardsMap.values.count { occurances -> occurances == i }.toString())
-        }
-
-        score = score.reversed()
-    }
-
-    override fun toString(): String {
-        return "Cards: $cards Score: $score Bid: $bid | "
-    }
+    /***
+     * Find lcm of the steps for each path
+     */
+    println(stepsPerNode)
+    var result = findLCM(stepsPerNode.toIntArray())
+    return result
 
 }
-
-fun readFile(filename: String): Int {
-    var sum = 0
-
-    var handsList: MutableList<Hand> = mutableListOf()
-
-    File(filename).forEachLine { line ->
-        val cards = line.split(" ").first()
-        val bid = line.split(" ").last().toInt()
-        val hand = Hand(cards, bid)
-        handsList.add(hand)
-    }
-
-    handsList = handsList.sortedWith(compareBy<Hand> { it.score }.then(handCardsComparator)).toMutableList()
-    handsList.forEachIndexed { index, hand ->
-        println(hand.bid)
-//        println("POSITION: ${index + 1} hand $hand")
-
-        sum += (index + 1) * hand.bid
-    }
-    return sum
-
-}
-
 
 fun main(args: Array<String>) {
     val inputPath = Path("src/main/kotlin/day8/part2/input.txt").toString()
