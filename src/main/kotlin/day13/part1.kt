c4cdd808e7d9bf5f040056182e1b4fa30b5abcfc
Erik <e.fakin3@gmail.com>
2023-12-22 00:13:36 +0100

    day 13 part 2

--- src/main/kotlin/day13/part1.kt
+++ src/main/kotlin/day13/part1.kt
@@ -7,35 +7,34 @@ fun isMirroredHorizontally(pattern: MutableList<String>, index: Int): Boolean {
     val maxOffset = min(index, pattern.size - (index + 2))
     for (i in 0..maxOffset) {
         if (pattern[index - i] != pattern[index + i + 1]) {
-            println("${pattern[index - i]} and ${pattern[index + i + 1]} are not equal" )
             return false
         }
     }
-    return true
 
+    return true
 }
+
 fun isMirroredVertically(pattern: MutableList<String>, index: Int): Boolean {
     val maxOffset = min(index, pattern.first().length - (index + 2))
     for (i in 0..maxOffset) {
-        for (j in 0 ..< pattern.size) {
+        for (j in 0..<pattern.size) {
             if (pattern[j][index - i] != pattern[j][index + i + 1]) {
-                   println("${pattern[j][index - i]} and ${pattern[j][index + i + 1]} are not equal" )
-
                 return false
             }
         }
     }
-    return true
 
+    return true
 }
+
 fun main() {
     var patterns = mutableListOf(mutableListOf<String>())
     var patternCounter = 0
-    val input = File("src/main/kotlin/day13/input.txt").forEachLine { line ->
-        if (line =="") {
+    File("src/main/kotlin/day13/input.txt").forEachLine { line ->
+        if (line == "") {
             patternCounter++
             patterns.add(mutableListOf<String>())
-        }   else {
+        } else {
             patterns[patternCounter].add(line)
         }
     }
@@ -44,35 +43,24 @@ fun main() {
 
     patterns.forEachIndexed { index, pattern ->
         for (i in 0..<pattern.size - 1) {
-
-           if (pattern[i] == pattern[i+1]) {
-
-               if (isMirroredHorizontally(pattern, i)) result += (i + 1) * 100
-
-           }
+            if (pattern[i] == pattern[i + 1]) {
+                if (isMirroredHorizontally(pattern, i)) result += (i + 1) * 100
+            }
         }
 
         for (i in 0..<pattern.first().length - 1) {
             for (j in 0..<pattern.size - 1) {
-                if (pattern[j][i] != pattern[j][i+1]) {
+                if (pattern[j][i] != pattern[j][i + 1]) {
                     break
                 }
                 if (j == pattern.size - 2) {
-
-                    if(isMirroredVertically(pattern, i)) {result += i + 1}
-
+                    if (isMirroredVertically(pattern, i)) {
+                        result += i + 1
+                    }
                 }
             }
         }
-
-
     }
 
     println("Result: $result")
-
-
-
-
-
-
 }
--- src/main/kotlin/day13/part2.kt
+++ src/main/kotlin/day13/part2.kt
@@ -3,91 +3,79 @@ package day13_2
 import java.io.File
 import kotlin.math.min
 
-fun isMirroredHorizontally(pattern: MutableList<String>, index: Int): Boolean {
+fun findSmudgeHorizontally(pattern: MutableList<String>, index: Int): MutableList<MutableList<Int>> {
     val maxOffset = min(index, pattern.size - (index + 2))
-    for (i in 0..maxOffset) {
-        if (pattern[index - i] != pattern[index + i + 1]) {
-            println("${pattern[index - i]} and ${pattern[index + i + 1]} are not equal" )
-            return false
-        }
-    }
-    return true
+    var differencesIndex = mutableListOf(mutableListOf<Int>())
 
-}
-fun isMirroredVertically(pattern: MutableList<String>, index: Int): Boolean {
-    val maxOffset = min(index, pattern.first().length - (index + 2))
     for (i in 0..maxOffset) {
-        for (j in 0 ..< pattern.size) {
-            if (pattern[j][index - i] != pattern[j][index + i + 1]) {
-                println("${pattern[j][index - i]} and ${pattern[j][index + i + 1]} are not equal" )
-
-                return false
+        for (j in 0..<pattern.first().length) {
+            if (pattern[index - i][j] != pattern[index + i + 1][j]) {
+                differencesIndex.add(mutableListOf(j, index - i))
             }
         }
     }
-    return true
 
+    return differencesIndex
 }
 
-fun findSmudge(pattern: MutableList<String>, index: Int): IntArray{
+fun findSmudgeVertically(pattern: MutableList<String>, index: Int): MutableList<MutableList<Int>> {
+    val maxOffset = min(index, pattern.first().length - (index + 2))
+    var differencesIndex = mutableListOf(mutableListOf<Int>())
 
+    for (i in 0..maxOffset) {
+        for (j in 0..<pattern.size) {
+            if (pattern[j][index - i] != pattern[j][index + i + 1]) {
+                differencesIndex.add(mutableListOf(index - i, j))
+            }
+        }
+    }
 
-
+    return differencesIndex
 }
+
 fun main() {
     var patterns = mutableListOf(mutableListOf<String>())
     var patternCounter = 0
-    val input = File("src/main/kotlin/day13/input.txt").forEachLine { line ->
-        if (line =="") {
+    File("src/main/kotlin/day13/input.txt").forEachLine { line ->
+        if (line == "") {
             patternCounter++
             patterns.add(mutableListOf<String>())
-        }   else {
+        } else {
             patterns[patternCounter].add(line)
         }
     }
 
     var result = 0
 
-    var smudgesLocations = mutableListOf(intArrayOf())
 
-    patterns.forEachIndexed{ index, pattern ->
+    var horizontalLines = mutableListOf<Int>()
+    var verticalLines = mutableListOf<Int>()
 
-        println("Index $index smudge at ${findSmudge(pattern).first()} ${findSmudge(pattern).last()}")
+    patterns.forEach { pattern ->
 
-    }
-
-
-    patterns.forEachIndexed { index, pattern ->
         for (i in 0..<pattern.size - 1) {
-
-            if (pattern[i] == pattern[i+1]) {
-
-                if (isMirroredHorizontally(pattern, i)) result += (i + 1) * 100
-
+            val smudges = findSmudgeHorizontally(pattern, i)
+            if (smudges.size - 1 == 1) {
+                horizontalLines.add(i + 1)
+                return@forEach
             }
         }
 
         for (i in 0..<pattern.first().length - 1) {
             for (j in 0..<pattern.size - 1) {
-                if (pattern[j][i] != pattern[j][i+1]) {
-                    break
-                }
-                if (j == pattern.size - 2) {
-
-                    if(isMirroredVertically(pattern, i)) {result += i + 1}
-
+                val smudges = findSmudgeVertically(pattern, i)
+                if (smudges.size - 1 == 1) {
+                    verticalLines.add(i + 1)
+                    return@forEach
                 }
             }
         }
 
-
     }
 
-    println("Result: $result")
+    result += horizontalLines.sum() * 100
+    result += verticalLines.sum()
 
-
-
-
-
+    println("Result $result")
 
 }
